564730d62b1a3c6ca136befcd014824ccf1c012a
Abhishek Tiwari <abhishri73555@gmail.com>
2026-07-07 20:06:50 +0530

    Time: 0 ms (100%), Space: 8.9 MB (89.09%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     long long sumAndMultiply(int n) {
-        long long x = 0;
-        long long sum = 0;
-        long long multiplier = 1; // Tracks the place value from right to left
-
-        while (n > 0) {
-            int digit = n % 10;
-            n /= 10;
-
-            if (digit != 0) {
-                sum += digit;
-                x = digit * multiplier + x; // Place the digit at the correct left-most position
-                multiplier *= 10;           // Shift position for the next non-zero digit
+        long long x=0;
+        long long sum=0;
+        long long multiplier=1; 
+        while(n>0){
+            int digit=n%10;
+            n/=10;
+            if(digit!=0){
+                sum+=digit;
+                x=digit*multiplier+x; 
+                multiplier*=10;
             }
         }
-        
-        return x * sum;
+        return x*sum;
     }
 };
