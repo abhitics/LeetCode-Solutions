6ea7b41226f72f9576682dba468f2aecbbe48b2d
Abhishek Tiwari <abhishri73555@gmail.com>
2026-07-18 22:06:34 +0530

    Time: 55 ms (30.54%), Space: 81.5 MB (93.75%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
-
-        // int n = nums.size();
-        // sort(nums.begin(), nums.end());
-        // vector<int> dp(n,1);
-        // int ans = 1;
-        // for(int i = 1; i < n; i++){
-        //     for(int j = 0; j < i; j++){
-        //         if(nums[j][0] < nums[i][0] &&
-        //            nums[j][1] < nums[i][1]){
-        //             dp[i] = max(dp[i], dp[j] + 1);
-        //         }
-        //     }
-        //     ans = max(ans, dp[i]);
-        // }
-
         sort(envelopes.begin(), envelopes.end(),
         [](vector<int>& a, vector<int>& b){
             if(a[0] == b[0])
@@ -34,5 +19,3 @@ public:
         return lis.size();
     }
 };
-
-       
