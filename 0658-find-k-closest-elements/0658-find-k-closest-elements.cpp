f45705105ac586710804b92bc1382d4cb15f41fd
Abhishek Tiwari <abhishri73555@gmail.com>
2026-07-18 21:12:26 +0530

    Time: 0 ms (100%), Space: 35.9 MB (66.66%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int n=arr.size();
+        int lo=0;
+        int hi=n-k;
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2;
+
+            if((x -arr[mid])> (arr[mid + k]-x))  lo = mid + 1;
+            else hi = mid;
+        }
+        return vector<int>(arr.begin() + lo, arr.begin() + lo + k);
+    }
+};
